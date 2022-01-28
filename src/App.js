2e2641e9f89102c44eb97f544ad43a48ab28a30e
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-29 04:51:52 +0530

    register route checked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 function App() {
-	return <Login />;
+	return <Register />;
 }
 
 export default App;
