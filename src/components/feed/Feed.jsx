f7c096622ef236184abcc85d000bceae7b5a9a6f
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-29 01:40:10 +0530

    imported share

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,5 +1,11 @@
+import Share from "../share/share";
 import "./feed.css";
-
 export default function Feed() {
-	return <div className="feed"></div>;
+	return (
+		<div className="feed">
+			<div className="feedWrapper">
+				<Share />
+			</div>
+		</div>
+	);
 }
