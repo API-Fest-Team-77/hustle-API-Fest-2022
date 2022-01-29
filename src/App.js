bb26135d821bbd8335deb470bf75c8a322543537
piyusha2001 <ppiyusha2001@gmail.com>
2022-01-29 11:28:38 +0530

    routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login.jsx";
+import Profile from "./pages/profile/Profile.jsx";
+import Register from "./pages/register/Register.jsx";
+
 function App() {
-	return <Home />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="login" element={<Login />} />
+				<Route path="register" element={<Register />} />
+				<Route path="profile/:username" element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
